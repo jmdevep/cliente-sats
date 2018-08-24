0f12ab9158e53f0b90a36bd338fdb1b8139fb8e3
jmdevep <julianmontesdeocacastillo@gmail.com>
2018-08-24 18:09:31 -0300

    Registro Trmao

---
+++ src/components/tramo/RegistroTramo.vue
@@ -0,0 +1,161 @@
+<template>
+    <div>
+        <h1 class="mainTitle">
+        Tramos
+        </h1>
+        <hr class="titleUnderline">
+        <div class="card border-success mb-3">
+            <div class="card-header greenBackground">Registro de Tramos</div>
+            <div class="card-body darkTextCustom">
+                <form v-on:submit.prevent="registrarTramo()">
+                    <p v-if="erroresForm.length">
+                        <b>Por favor corrija lo siguiente:</b>
+                        <ul>
+                            <li v-for="(error, index) in erroresForm" :key="index">{{ error }}</li>
+                        </ul>
+                    </p>
+                    <i v-show="loading" class="fa fa-spinner fa-spin"></i>
+                    <p>{{ resultadoOperacion }}</p>
+                    <label>Seleccionar localidad de Origen:</label>
+                    <select class="form-control" v-model="tramo.localidadOrigen" @change="cambioSelect">
+                        <option v-for="(localidad,index) in localidades" :key="index" v-bind:value="localidad">
+                            {{ localidad.nombre }}
+                        </option>
+                    </select>
+                    <label>Seleccionar localidad de Destino:</label>
+                    <select class="form-control" v-model="tramo.localidadDestino" @change="cambioSelect">
+                        <option v-for="(localidad,index) in localidades" :key="index" v-bind:value="localidad">
+                            {{ localidad.nombre }}
+                        </option>
+                    </select>
+                    <div class="form-group">
+                        <label for="nombre" class="darkTextCustom">Cantidad de Kilómetros</label>
+                        <input type="text" class="form-control border-success" v-model="tramo.cantidadKm" id="cantidadKm" placeholder="Cantidad km">
+                    </div>
+                    <input type="submit" :disabled="disabled" value="Registrar" class="btn marginBefore tableHeadingBackground">
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+	import axios from 'axios';
+	 export default {
+        name: 'RegistroTramo',
+        mounted(){
+            this.loading = true;
+            axios.get('http://localhost:4567/api/localidad/lista-localidades')
+        		.then((res)=>{
+                    console.log(res);
+        			if(res.data.resultado == 100){
+                        this.tiposUsuario = res.data.tiposUsuario;
+                    }
+                    this.loading = false;
+        	});
+            
+            },
+        beforeCreate: function () {
+            var usuario = this.$session.get('usuario');
+            if (!this.$session.exists() || usuario == null || usuario.tipo.id != 2) {
+            this.$router.push('/usuario/login')
+            } 
+        },
+        data(){
+            return{
+                loading: false,
+                resultadoOperacion: '',
+                erroresForm: [],
+                disabled: false,
+            	usuario: {
+                    nombre: '',
+                    contrasena: '',
+                    tipo: {
+                        id: 0,
+                    },
+                    idEmpleado: 0,
+                },
+                tiposUsuario: [],
+                empleados: [],
+                errorDisponibilidad: '',
+            }
+
+        },
+        methods: {
+            cambioSelect () {
+                    if(this.usuario.tipo.id == 1){
+                        this.cargarEmpleadosDisponibles();
+                    }
+            },
+            verificarDisponibilidad() {
+                if(this.usuario.nombre != ''){
+                    axios.get('http://localhost:4567/api/usuario/existe-usuario', {
+                        params: {
+                        nombreUsuario: this.usuario.nombre,
+                        }
+                    })
+                        .then((res)=>{
+                            console.log(res);
+                            if(res.data.existe == true){
+                                this.disabled = true;
+                                this.errorDisponibilidad = "Nombre de usuario ya se encuentra en uso.";
+                            } else {
+                                this.errorDisponibilidad = "Nombre de usuario disponible.";    
+                                this.disabled = false;                              
+                            }
+                    });
+                }
+            },
+            registrarUsuario(){
+                this.loading = true;
+                if(this.checkForm()){
+                    var params = this.usuario;
+                    console.log(params);
+                    axios.post('http://localhost:4567/api/usuario/agregar-usuario', params) 
+                        .then((res)=>{
+                            console.log(res.data.resultado);                            
+                            if(res.data.resultado == 1102){
+                                this.resultadoOperacion = "Usuario agregado satisfactoriamente.";
+                                this.limpiarCajas();
+                            } else if (res.data.resultado == 1100){
+                                this.resultadoOperacion = "Ya existe un usuario con ese nombre.";
+                            }
+                        });
+                    this.loading = false;
+                }
+
+            },
+            limpiarCajas(){
+                this.usuario.nombre = '';
+                this.usuario.contrasena = '';
+                this.usuario.tipo.id = 0;
+                this.usuario.idEmpleado = 0;
+                this.empleados = this.cargarEmpleadosDisponibles();
+                errorDisponibilidad: '';
+            },
+            cargarEmpleadosDisponibles(){
+
+            },
+            checkForm() {
+                if (this.usuario.nombre && this.usuario.contrasena && this.usuario.tipo.id != 0) {
+                    return true;
+                }
+
+                this.erroresForm = [];
+
+                if (!this.usuario.nombre) {
+                    this.erroresForm.push('Nombre requerido.');
+                }
+                if (!this.usuario.contrasena) {
+                    this.erroresForm.push('Contraseña requerida.');
+                }
+                if (!this.usuario.tipo.id == 0) {
+                    this.erroresForm.push('Debes seleccionar un tipo de usuario.');
+                }
+                this.disabled = false;
+                return false;
+            }
+    
+        },    
+    }
+</script>
