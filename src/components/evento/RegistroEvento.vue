b5604c2727f2b9c2f3ed9929a0f8e6d09a8da688
jmdevep <julianmontesdeocacastillo@gmail.com>
2018-08-31 00:18:30 -0300

    Registro Evento

--- src/components/evento/RegistroEvento.vue
+++ src/components/evento/RegistroEvento.vue
@@ -1,11 +1,7 @@
 <template>
     <div>
-        <h1 class="mainTitle">
-        Tramos
-        </h1>
-        <hr class="titleUnderline">
         <div class="card border-success mb-3">
-            <div class="card-header greenBackground">Registro de Tramos</div>
+            <div class="card-header greenBackground">Registro de Eventos</div>
             <div class="card-body darkTextCustom">
                 <form v-on:submit.prevent="registrarEvento()">
                     <p v-if="erroresForm.length">
@@ -16,29 +12,64 @@
                     </p>
                     <i v-show="loading" class="fa fa-spinner fa-spin"></i>
                     <p>{{ resultadoOperacion }}</p>
+
                     
-                    <div id="project-label">Select a project (type "j" for a start):</div>
-                    <input id="project" />
-                    <input type="hidden" id="project-id" />
-                    <p id="project-title"></p><p id="project-description"></p><p id="project-other"></p>
+                    <multi-select v-model="personaSeleccionada" placeholder="Personas"  :optionsLimit="3" :tabindex="1"  track-by="nombre" :options="personas" :option-height="104" :custom-label="customLabelPersonas" :show-labels="false">
+                        
+                        <template slot="option" slot-scope="props">
+                            <div class="option__desc">
+                                <span class="option__title">{{ props.option.nombre }}</span>
+                                <br>
+                                <span class="option__small">{{ props.option.documento }}</span>
+                            </div>
+                        </template>
+                    </multi-select>
+                    <br> 
+                    
+                    <multi-select v-model="servicioSeleccionado" placeholder="Servicios" :optionsLimit="3" :tabindex="2" track-by="nombre" :options="servicios" :option-height="104" :custom-label="customLabelServicios" :show-labels="false">
+                        
+                        <template slot="option" slot-scope="props">
+                            <div class="option__desc">
+                                <span class="option__title">{{ props.option.nombre }}</span>
+                                <br>
+                                <span class="option__small">{{ props.option.descripcion }}</span>
+                            </div>
+                        </template>
+                    </multi-select>
+                    <br>
 
-                    <label>Seleccionar localidad de Destino:</label>
-                    <select class="form-control" v-model="tramo.localidadDestino" @change="cambioSelectDestino()">
-                        <option v-for="(localidad,index) in localidades" :disabled="localidad.disabled" :key="index" v-bind:value="localidad">
-                            {{ localidad.nombre }}
-                        </option>
-                    </select>
-                    <p v-if="tramo.localidadDestino">
-                        <b>Destino Seleccionado:</b>
-
-                            {{ tramo.localidadDestino.nombre }} 
-                                <a href="#" @click="removerLocalidadDestino()">
-                                    - <i class="fas fa-trash-alt"></i></a>
-                    </p>
+                    <multi-select v-model="tipoEventoSeleccionado" placeholder="Tipo Evento"  :optionsLimit="3" :tabindex="3" track-by="nombre" :options="tiposEventos" :option-height="104" :custom-label="customLabelTiposEventos" :show-labels="false">
+                        
+                        <template slot="option" slot-scope="props">
+                            <div class="option__desc">
+                                <span class="option__title">{{ props.option.nombre }}</span>
+                                <br>
+                                <span class="option__small">{{ props.option.descripcion }}</span>
+                            </div>
+                        </template>
+                    </multi-select>
+                
                     <div class="form-group">
-                        <label for="nombre" class="darkTextCustom">Cantidad de Kilómetros</label>
-                        <input type="number" class="form-control border-success" v-model="tramo.cantidadKm" id="cantidadKm" placeholder="Cantidad km">
+                        <label for="fecha" class="darkTextCustom">Fecha</label>
+                        <input type="date" class="form-control border-success" v-model="fechaInicio" id="fecha" placeholder="2019-12-05">
                     </div>
+                    <div class="form-group">
+                        <label for="horaI" class="darkTextCustom">Hora Inicio</label>
+                        <input type="time"  class="form-control border-success" id="horaI" v-model="horaInicio"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="fechaF" class="darkTextCustom">Fecha Finalización</label>
+                        <input type="date" class="form-control border-success" v-model="fechaFin" id="fechaF" placeholder="2019-12-05">
+                    </div>
+                    <div class="form-group">
+                        <label for="horaF" class="darkTextCustom">Hora Finalización (Opcional) </label>
+                        <input type="time"  class="form-control border-success" id="horaF" v-model="horaFin"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="direccion" class="darkTextCustom">Dirección</label>
+                        <input type="text" class="form-control border-success" v-model="evento.direccion" id="direccion" placeholder="direccion">
+                    </div>
+
                     <input type="submit" :disabled="disabled" value="Registrar" class="btn marginBefore tableHeadingBackground">
                 </form>
             </div>
@@ -47,25 +78,19 @@
 </template>
 
 <script>
-	import axios from 'axios';
+    import axios from 'axios';
+    import Multiselect from 'vue-multiselect'
+
+
 	 export default {
         name: 'RegistroEvento',
+        components: { 'multi-select': Multiselect,                      
+        },
         mounted(){
             this.loading = true;
-                       axios.get('https://servidor-sats.herokuapp.com/api/localidad/lista-localidades', {
-                params: {
-
-                }
-            })
-        		.then((res)=>{
-                    console.log(res);
-        			if(res.data.resultado == 100){
-                        this.localidades = res.data.listaLocalidades;
-                        this.localidades.forEach(function(obj) { obj.disabled = false; });
-                    }
-                    this.loading = false;
-        	});
-            
+            this.cargarPersonas();
+            this.cargarServicios();
+            this.cargarTiposEventos();
             },
         beforeCreate: function () {
             var usuario = this.$session.get('usuario');
@@ -75,80 +100,140 @@
         },
         data(){
             return{
+                personaSeleccionada: null,
+                servicioSeleccionado: null,
+                tipoEventoSeleccionado: null,
                 loading: false,
                 resultadoOperacion: '',
                 erroresForm: [],
                 disabled: false,
             	localidades: [],
                 errorDisponibilidad: '',
-                tramo: {
-                    localidadOrigen: null,
-                    localidadDestino: null,
-                    cantidadKm: null
+                evento: {
+                    inicioEvento: '',
+                    direccion: '',
+                    persona: null,
+                    servicio: null,
+                    tipo: null,
+                    direccion: '',
+                    finEvento: ''
                 },
+                personas: [],
+                servicios: [],
+                tiposEventos: [],
+                horaInicio: null,
+                horaFin: null,
+                fechaInicio: null,
+                fechaFin: null,
             }
         },
         methods: {
-            cambioSelectOrigen() {
-                var indexRol = this.localidades.findIndex((x => x.id == this.tramo.localidadOrigen.id));
-                this.localidades[indexRol].disabled = true;
+            customLabelPersonas ({ nombre, documento }) {
+                return `${nombre} – ${documento}`
             },
-            cambioSelectDestino() {
-                var indexRol = this.localidades.findIndex((x => x.id == this.tramo.localidadDestino.id));
-                this.localidades[indexRol].disabled = true;
+            customLabelServicios ({ nombre, descripcion }) {
+                return `${nombre} – ${descripcion}`
             },
-            removerLocalidadOrigen(){
-                var index = this.localidades.findIndex((x => x.id == this.tramo.localidadOrigen.id));
-                this.localidades[index].disabled = false;
-                this.tramo.localidadOrigen = null;
+            customLabelTiposEventos ({ nombre }) {
+                return `${nombre}`
             },
-            removerLocalidadDestino(){
-                var index = this.localidades.findIndex((x => x.id == this.tramo.localidadDestino.id));
-                this.localidades[index].disabled = false;
-                this.tramo.localidadDestino = null;
+            cargarPersonas(){
+                axios.get('https://servidor-sats.herokuapp.com/api/cliente/lista-personas', {
+                params: {
+                    }
+                })
+        		.then((res)=>{
+                    console.log(res);
+        			if(res.data.resultado == 100){
+                        this.personas = res.data.listaPersonas;
+                    }
+                    this.loading = false;
+        	    });         
             },
-            registrarTramo(){
+            cargarServicios(){
+                axios.get('https://servidor-sats.herokuapp.com/api/servicio/lista-servicios', {
+                params: {
+                    }
+                })
+        		.then((res)=>{
+                    console.log(res);
+        			if(res.data.resultado == 100){
+                        this.servicios = res.data.listaServicios;
+                    }
+                    this.loading = false;
+        	    });         
+            },
+            cargarTiposEventos(){
+                axios.get('https://servidor-sats.herokuapp.com/api/evento/lista-tipos-eventos', {
+                params: {
+                    }
+                })
+        		.then((res)=>{
+                    console.log(res);
+        			if(res.data.resultado == 100){
+                        this.tiposEventos = res.data.listaTiposEventos;
+                    }
+                    this.loading = false;
+        	    });         
+            },
+            registrarEvento(){
+                this.evento.inicioEvento  = moment(`${this.fechaInicio} ${this.horaInicio}`, 'YYYY-MM-DD HH:mm:ss').format();
+                this.evento.finEvento  = moment(`${this.fechaFin} ${this.horaFin}`, 'YYYY-MM-DD HH:mm:ss').format();
                 this.loading = true;
+
+                delete this.personaSeleccionada.fechaNacimiento;
+
                 if(this.checkForm()){
-                    this.tramo.localidadOrigen.disabled = undefined;
-                    this.tramo.localidadDestino.disabled = undefined;
-                    var params = this.tramo;
+                    this.evento.persona = this.personaSeleccionada;
+                    this.evento.tipo = this.tipoEventoSeleccionado;
+                    this.evento.servicio = this.servicioSeleccionado;
+
+                    var params = this.evento;
                     console.log(params);
-                    axios.post('https://servidor-sats.herokuapp.com/api/tramo/agregar-tramo', params) 
+                    
+                    axios.post('http://localhost:4567/api/evento/agregar-evento', params) 
                         .then((res)=>{
                             console.log(res.data.resultado);                            
-                            if(res.data.resultado == 5702){
-                                this.resultadoOperacion = "Tramo agregado satisfactoriamente.";
-                                this.limpiarCajas();
-                            } else if (res.data.resultado == 5703){
-                                this.resultadoOperacion = "Ya existe un tramo con esas localidades.";
+                            if(res.data.resultado == 5802){
+                                this.resultadoOperacion = "Evento agregado satisfactoriamente.";
+                                this.limpiarCajas();    
+                            } else if (res.data.resultado == 5803){
+                                this.resultadoOperacion = "Error en el alta.";
                             }
-                        });
+                        }); 
                     this.loading = false;
                 }
             },
             limpiarCajas(){
-                this.tramo.localidadOrigen = '';
-                this.tramo.localidadDestino = '';
-                this.tramo.cantidadKm = 0;
                 errorDisponibilidad: '';
             },
             checkForm() {
-                if (this.tramo.localidadOrigen && this.tramo.localidadDestino && this.tramo.cantidadKm != 0) {
+                if (this.fechaInicio && this.fechaFin  && this.horaInicio && this.horaFin  && this.direccion != ''
+                && this.personaSeleccionada != null && this.servicioSeleccionado !=  null && this.tipoEventoSeleccionado != null) {
                     return true;
                 }
 
                 this.erroresForm = [];
+                
+                if (!this.fechaInicio) {
+                    this.erroresForm.push('Fecha inicio requerida.');
+                } 
+                if(!this.horaInicio){
+                    this.erroresForm.push('Hora Inicio requerida');
+                }
+                if(this.direccion == ''){
+                    this.erroresForm.push('Direccion requerida');
+                }
+                if(!this.personaSeleccionada){
+                    this.erroresForm.push("Paciente requerido");
+                }
+                if(!this.servicioSeleccionado){
+                    this.erroresForm.push('Servicio requerido');
+                }
+                if(!this.tipoEventoSeleccionado){
+                    this.erroresForm.push('Tipo de Evento requerido');
+                }
 
-                if (!this.tramo.localidadOrigen) {
-                    this.erroresForm.push('Origen requerido.');
-                }
-                if (!this.tramo.localidadDestino) {
-                    this.erroresForm.push('Destino requerido.');
-                }
-                if (!this.tramo.cantidadKm == 0) {
-                    this.erroresForm.push('Cantidad de Kilómetros requerida.');
-                }
                 this.disabled = false;
                 return false;
             }
@@ -156,84 +241,5 @@
         },    
     }
 
-    $(function() {
-    var projects = [
-        {
-            value: "jquery",
-            label: "Julian Montes de Oca",
-            ci: "48640076",
-            socio: "Hospital Evengelico"
-        },
-        {
-            value: "fan buitres",
-            label: "Agostina Sol Camacho",
-            ci: "48659878",
-            socio: "Hospital Evangelico"
-        },
-        {
-            value: "sizzlejs",
-            label: "Sizzle JS",
-            ci: "a pure-JavaScript CSS selector engine",
-            socio: "49857 2389442 573489057 89024375 928037890"
-        }
-    ];
-    
-    function lightwell(request, response) {
-        function hasMatch(s) {
-            return s.toLowerCase().indexOf(request.term.toLowerCase())!==-1;
-        }
-        var i, l, obj, matches = [];
-
-        if (request.term==="") {
-		    response([]);
-            return;
-        }
-           
-        for  (i = 0, l = projects.length; i<l; i++) {
-            obj = projects[i];
-            if (hasMatch(obj.label) || hasMatch(obj.ci) || hasMatch(obj.value)) {
-                matches.push(obj);
-            }
-        }
-        response(matches);
-    }
-    
-    $( "#project" ).autocomplete({
-        minLength: 0,
-        source: lightwell,
-        focus: function( event, ui ) {
-            $( "#project" ).val( ui.item.label );
-            return false;
-        },
-        select: function( event, ui ) {
-            $( "#project" ).val( ui.item.label );
-            $( "#project-id" ).val( ui.item.value ); // is this needed?
-            $( "#project-title" ).html( ui.item.label );
-            $( "#project-description" ).html( ui.item.desc );
-            $( "#project-other" ).html( ui.item.other );
-            
-            return false;
-        }
-    })
-    
-    .data( "ui-autocomplete" )._renderItem = function( ul, item ) {
-        return $( "<li>" )
-            .append( "<a>" + item.label + 
-                "<br><span style='font-size: 80%;'>Ci: " + item.ci + "</span>" +
-                "<br><span style='font-size: 60%;'>Socio: " + item.socio + "</span></a>" )
-            .appendTo( ul );
-    };
-});
-
 </script>
-<style>
-#project-label {
-    display: block;
-    font-weight: bold;
-    margin-bottom: 1em;
-}
-#project-description {
-    margin: 0;
-    padding: 0;
-}
-</style>
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
